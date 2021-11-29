9657995f82b4df2b76698d450f0e905ea756b1bb
Mathankumar <bmathank555@email.com>
2021-11-29 20:31:22 +0530

    personal_info

---
+++ personal_info.php
@@ -0,0 +1,29 @@
+<?php
+
+$array = [
+   
+    'name'     =>  'Mathankumar',
+
+    'Email'    =>  'bmathan777@gmail.com',
+   
+    'address_1' =>array(
+    
+        'Door no'     => '123',
+        'Street Name '=> 'Anna st',
+        'Area'       => 'Puthur',
+        'City'       => 'Vellakovil'
+        
+    ),
+    
+    'address_2' =>array(
+    
+        'Door no'     => '123',
+        'Street Name '=> 'Anna st',
+        'Area'       => 'Puthur',
+        'City'       => 'Vellakovil'
+    )
+];
+
+print_r($array); die();
+
+?>
