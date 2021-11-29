5bf7c33555ed9676a15f5db716bc6b946d19a5f2
Mathankumar <bmathank555@email.com>
2021-11-29 19:56:51 +0530

    factorial

---
+++ factorial.php
@@ -0,0 +1,18 @@
+<?php
+$s_value = 10; 
+
+$e_value = 20; 
+$array = array();
+for($m=$s_value; $m<=$e_value; $m++){
+
+    for($k=1; $k<=$m; $k++){
+
+            array_push($array,$k);
+    }
+    $ans = array_product($array);
+     echo $ans."\n";
+     $array = array();
+     
+}
+
+?>
