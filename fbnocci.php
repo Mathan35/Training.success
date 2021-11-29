c92e5f9c39aceed3a245a9e7d393f5009e1390d9
Mathankumar <bmathank555@email.com>
2021-11-29 16:23:34 +0530

    fibnocci.2

--- fbnocci.php
+++ fbnocci.php
@@ -5,9 +5,10 @@ $n = 0;
 $m = 1;
 echo $n."\n";
 echo $m."\n";
-while($m<15){
+while($m<13){
     $ans = $n+$m;
-    echo $ans<15? $ans."\n":'';
+    // echo $ans<15? $ans."\n":'';
+    echo $ans."\n";
     $n = $m;
     $m = $ans;
 }
