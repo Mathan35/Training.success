8e298d02cf2d4a3ce23932fe6bff26b4f560b8bd
Mathankumar <bmathank555@email.com>
2021-11-29 20:52:03 +0530

    calcuator

---
+++ calculator.php
@@ -0,0 +1,21 @@
+<?php
+
+function add($a,$b){
+    return $a+$b;
+}
+function subract($a,$b){
+    return $a-$b;
+}
+function product($a,$b){
+    return $a*$b;
+}
+function division($a,$b){
+    return $a/$b;
+}
+
+echo add(10,20)."\n";
+echo subract(10,20)."\n";
+echo product(10,20)."\n";
+echo division(10,20)."\n";
+
+?>
