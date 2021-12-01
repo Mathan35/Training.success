26a2c1eefb8bc0af171bcfd2fd6c1d8f76af67bf
Mathankumar <bmathank555@email.com>
2021-12-01 09:30:24 +0530

    page_update

--- auth/login.php
+++ auth/login.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Login page</title>
 </head>
-
 <body>
 
 <div style = "border:1px solid black; width:fit-content; margin:30px;">
